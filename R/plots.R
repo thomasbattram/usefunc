31923ce4eb17a5e7017234836a0d2c1d5624dd0c
Thomas Battram <tb13101@bristol.ac.uk>
2019-01-21 13:39:59 +0000

    Added a function to produce manhattan plots using ggplot

--- R/plots.R
+++ R/plots.R
@@ -148,6 +148,77 @@ met_num <- nrow(dat)/group_num/col_num
 }
 
 
+#' Manhattan plot function for gwas (stolen from pcgoddard)
+#' 
+#' Generates a Manhattan plot using gwas data
+#' 
+#' @param df data.frame containing your results
+#' @param threshold the P value threshold
+#' @param hlight a vector containing any SNPs that need highlighting
+#' @param col colour palette used
+#' @param ylims limits on y-axis
+#' @param title title of the plot
+gg.manhattan <- function(df, threshold, hlight, col, ylims, title){
+  # format df
+  df.tmp <- df %>% 
+    
+    # Compute chromosome size
+    group_by(CHR) %>% 
+    summarise(chr_len=max(BP)) %>% 
+    
+    # Calculate cumulative position of each chromosome
+    mutate(tot=cumsum(chr_len)-chr_len) %>%
+    dplyr::select(-chr_len) %>%
+    
+    # Add this info to the initial dataset
+    left_join(df, ., by=c("CHR"="CHR")) %>%
+    
+    # Add a cumulative position of each SNP
+    arrange(CHR, BP) %>%
+    mutate( BPcum=BP+tot) %>%
+    
+    # Add highlight and annotation information
+    mutate( is_highlight=ifelse(SNP %in% hlight, "yes", "no")) %>%
+    mutate( is_annotate=ifelse(P < threshold, "yes", "no"))
+  
+  # get chromosome center positions for x-axis
+  axisdf <- df.tmp %>% group_by(CHR) %>% summarize(center=( max(BPcum) + min(BPcum) ) / 2 )
+  
+  ggplot(df.tmp, aes(x=BPcum, y=-log10(P))) +
+    # Show all points
+    geom_point(aes(color=as.factor(CHR)), alpha=0.8, size=2) +
+    scale_color_manual(values = rep(col, 22 )) +
+
+    # custom X axis:
+    scale_x_continuous( label = axisdf$CHR, breaks= axisdf$center ) +
+    scale_y_continuous(expand = c(0, 0)) + # expand=c(0,0)removes space between plot area and x axis 
+    
+    # add plot and axis titles
+    ggtitle(paste0(title)) +
+    labs(x = "Chromosome") +
+    
+    # add genome-wide sig and sugg lines
+    geom_hline(yintercept = -log10(sig)) +
+    geom_hline(yintercept = -log10(sugg), linetype="dashed") +
+    
+    # Add highlighted points
+    #geom_point(data=subset(df.tmp, is_highlight=="yes"), color="orange", size=2) +
+    
+    # Add label using ggrepel to avoid overlapping
+    geom_label_repel(data=df.tmp[df.tmp$is_annotate=="yes",], aes(label=as.factor(SNP), alpha=0.7), size=5, force=1.3) +
+    
+    # Custom the theme:
+    theme_bw(base_size = 22) +
+    theme( 
+      plot.title = element_text(hjust = 0.5),
+      legend.position="none",
+      panel.border = element_blank(),
+      panel.grid.major.x = element_blank(),
+      panel.grid.minor.x = element_blank()
+    )
+}
+
+
 
 
 
