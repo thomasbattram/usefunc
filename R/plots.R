33b19260c1ffc4db0785528869df957bd41fa11f
Thomas Battram <tb13101@bristol.ac.uk>
2019-01-21 16:49:14 +0000

    updated the manhattan plot - not finished yet though

--- R/plots.R
+++ R/plots.R
@@ -158,44 +158,44 @@ met_num <- nrow(dat)/group_num/col_num
 #' @param col colour palette used
 #' @param ylims limits on y-axis
 #' @param title title of the plot
-gg.manhattan <- function(df, threshold, hlight, col, ylims, title){
+gg.manhattan <- function(df, threshold, hlight, col = brewer.pal(9, "Greys")[c(3,7)], ylims, title, SNP = "SNP", CHR = "CHR", BP = "BP", P = "P", sig = 1e-8, sugg = 1e-6){
   # format df
   df.tmp <- df %>% 
     
     # Compute chromosome size
-    group_by(CHR) %>% 
-    summarise(chr_len=max(BP)) %>% 
+    group_by_(CHR) %>% 
+    summarise_(chr_len = max(.[[BP]])) %>% 
     
     # Calculate cumulative position of each chromosome
-    mutate(tot=cumsum(chr_len)-chr_len) %>%
+    mutate(tot = cumsum(chr_len) - chr_len) %>%
     dplyr::select(-chr_len) %>%
     
     # Add this info to the initial dataset
-    left_join(df, ., by=c("CHR"="CHR")) %>%
+    left_join(df, ., by=setNames(CHR, CHR)) %>%
     
     # Add a cumulative position of each SNP
-    arrange(CHR, BP) %>%
-    mutate( BPcum=BP+tot) %>%
+    arrange_(CHR, BP) %>%
+    mutate( BPcum = !! as.name(BP) + tot) %>%
     
     # Add highlight and annotation information
-    mutate( is_highlight=ifelse(SNP %in% hlight, "yes", "no")) %>%
-    mutate( is_annotate=ifelse(P < threshold, "yes", "no"))
+    mutate( is_highlight = ifelse(!! as.name(SNP) %in% hlight, "yes", "no")) %>%
+    mutate( is_annotate = ifelse(!! as.name(P) < threshold, "yes", "no"))
   
   # get chromosome center positions for x-axis
-  axisdf <- df.tmp %>% group_by(CHR) %>% summarize(center=( max(BPcum) + min(BPcum) ) / 2 )
+  axisdf <- df.tmp %>% group_by_(CHR) %>% summarize(center=( max(BPcum) + min(BPcum) ) / 2 )
   
-  ggplot(df.tmp, aes(x=BPcum, y=-log10(P))) +
+  ggplot(df.tmp, aes(x = BPcum, y = -log10(get(P)))) +
     # Show all points
-    geom_point(aes(color=as.factor(CHR)), alpha=0.8, size=2) +
-    scale_color_manual(values = rep(col, 22 )) +
+    geom_point(aes_string(color = as.factor(CHR)), alpha=0.8, size=2) +
+    scale_color_manual(values = rep(col, 24)) +
 
     # custom X axis:
-    scale_x_continuous( label = axisdf$CHR, breaks= axisdf$center ) +
+    scale_x_continuous( label = axisdf[[CHR]], breaks= axisdf$center ) +
     scale_y_continuous(expand = c(0, 0)) + # expand=c(0,0)removes space between plot area and x axis 
     
     # add plot and axis titles
     ggtitle(paste0(title)) +
-    labs(x = "Chromosome") +
+    labs(x = "Chromosome", y = "-log10(P)") +
     
     # add genome-wide sig and sugg lines
     geom_hline(yintercept = -log10(sig)) +
@@ -205,7 +205,7 @@ gg.manhattan <- function(df, threshold, hlight, col, ylims, title){
     #geom_point(data=subset(df.tmp, is_highlight=="yes"), color="orange", size=2) +
     
     # Add label using ggrepel to avoid overlapping
-    geom_label_repel(data=df.tmp[df.tmp$is_annotate=="yes",], aes(label=as.factor(SNP), alpha=0.7), size=5, force=1.3) +
+    geom_label_repel(data=df.tmp[df.tmp$is_annotate=="yes",], aes_string(label=as.factor(SNP), alpha=0.7), size=5, force=1.3) +
     
     # Custom the theme:
     theme_bw(base_size = 22) +
