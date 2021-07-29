10ae676db29d7ade98d32cf483c858c5b845b880
Thomas Battram <thomas.battram@bristol.ac.uk>
2021-07-29 11:35:29 +0100

    added extracting sites to remove from zhou et al

---
+++ R/dnam-array-info.R
@@ -0,0 +1,43 @@
+#' get_zhou_recs
+#' 
+#' Extract CpGs to exclude from zhou et al. 2017 (http://zwdzwd.github.io/InfiniumAnnotation)
+#' Can extract just CpGs or the table with reasons for exclusion
+#' 
+#' @param outpath file path to output the data
+#' @param listonly a logical value. If 'TRUE', list of CpGs recommended to exlude are returned. If "FALSE", full table of CpGs with reasons for potentially removing them returned.
+#' @param array string. Array type being used, must be either "hm450" or "epic".
+#' @param overwrite a logical value. If 'TRUE' outpath is overwritten. If 'FALSE' it is not overwritten.
+#' @export
+#' @return Either a character vector of CpG sites to exclude from an array or a tibble with reasons to exlcude certain sites.
+get_zhou_recs <- function(outpath, listonly = TRUE, array = c("hm450", "epic"), overwrite = FALSE) {
+    ## Check output path
+    if (base::file.exists(file.path) && !overwrite) {
+        base::message(outpath, " is already present. Reading in from that file.")
+        if (listonly) return(base::readLines(outpath))
+        if (!listonly) return(readr::read_tsv(outpath))
+    } else {
+        ## Set download path
+        if (array %in% "hm450") {
+            download_path <- "http://zhouserver.research.chop.edu/InfiniumAnnotation/20180909/HM450/HM450.hg38.manifest.tsv.gz"
+            base::message("Downloading from ", download_path)
+        } else if (array == "epic") {
+            download_path <- "http://zhouserver.research.chop.edu/InfiniumAnnotation/20180909/EPIC/EPIC.hg38.manifest.tsv.gz"
+            base::message("Downloading from ", download_path)
+        } else {
+            stop("array must be either 'hm450' or 'epic'")
+        } 
+        ## Download the file using wget
+        download_status <- utils::download.file(url = download_path, 
+                                                destfile = outpath,
+                                                method = "wget")
+        if (download_status != 0) stop("Failed download. Try again or check the download path")
+        message("File downloaded to ", outpath)
+        ## return the results
+        res <- readr::read_tsv(outpath)
+        if (listonly) {
+            return(res[res$MASK_general, "probeID", drop = TRUE])
+        } else {
+            return(res)
+        }
+    }
+}
